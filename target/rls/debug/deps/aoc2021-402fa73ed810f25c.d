dd840c090b77794614b367275ffb58fce7d8d2b9
Adam Gatt <adam.gatt@live.com.au>
2021-12-03 22:02:36 +1030

    Remove build files

--- target/rls/debug/deps/aoc2021-402fa73ed810f25c.d
+++ target/rls/debug/deps/aoc2021-402fa73ed810f25c.d
@@ -1,9 +1,10 @@
-/home/adamgatt/coding/rust/aoc2021/target/rls/debug/deps/aoc2021-402fa73ed810f25c.rmeta: src/main.rs src/utils.rs src/solutions.rs src/solutions/solution1.rs src/solutions/solution2.rs
+/home/adamgatt/coding/rust/aoc2021/target/rls/debug/deps/aoc2021-402fa73ed810f25c.rmeta: src/main.rs src/utils.rs src/solutions.rs src/solutions/solution1.rs src/solutions/solution2.rs src/solutions/solution3.rs
 
-/home/adamgatt/coding/rust/aoc2021/target/rls/debug/deps/aoc2021-402fa73ed810f25c.d: src/main.rs src/utils.rs src/solutions.rs src/solutions/solution1.rs src/solutions/solution2.rs
+/home/adamgatt/coding/rust/aoc2021/target/rls/debug/deps/aoc2021-402fa73ed810f25c.d: src/main.rs src/utils.rs src/solutions.rs src/solutions/solution1.rs src/solutions/solution2.rs src/solutions/solution3.rs
 
 src/main.rs:
 src/utils.rs:
 src/solutions.rs:
 src/solutions/solution1.rs:
 src/solutions/solution2.rs:
+src/solutions/solution3.rs:
