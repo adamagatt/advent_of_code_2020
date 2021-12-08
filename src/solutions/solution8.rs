44783644e4e401d23984333e9cb9c69e0e439c0e
Adam Gatt <adam.gatt@live.com.au>
2021-12-08 22:34:23 +1030

    Improved naming

--- src/solutions/solution8.rs
+++ src/solutions/solution8.rs
@@ -2,8 +2,8 @@ use crate::utils::read_lines_by_words;
 
 use std::collections::HashSet;
 
-type CharSet = HashSet<char>;
-type ProblemLine = (Vec<CharSet>, Vec<CharSet>);
+type Segments = HashSet<char>;
+type ProblemLine = (Vec<Segments>, Vec<Segments>);
 
 const UNIQUE_LENGTHS_FOR_DIGIT: [(usize, usize); 4] = [(1, 2), (4, 4), (7, 3), (8, 7)];
 
@@ -28,13 +28,13 @@ fn solution8a(code_lines: Vec<ProblemLine>) -> usize {
 
 fn solution8b(mut code_lines: Vec<ProblemLine>) -> i32 {
     code_lines.iter_mut()
-        .map(|(inputs, outputs)| calc_output_values(determine_charset_map(inputs), outputs))
+        .map(|(inputs, outputs)| calc_output_values(determine_segment_map(inputs), outputs))
         .sum()
 }
 
-fn calc_output_values(charset_map: [Option<CharSet>; 10], outputs: &Vec<CharSet>) -> i32 {
+fn calc_output_values(segment_map: [Option<Segments>; 10], outputs: &Vec<Segments>) -> i32 {
     outputs.iter()
-        .map(|output| charset_map.iter().position(|charset| charset.as_ref().unwrap() == output))
+        .map(|output| segment_map.iter().position(|segment_set| segment_set.as_ref().unwrap() == output))
         // Reverse order of found digits so that the increasing enumeration can be used as the
         // exponent in the math of raising that digit to the correct power of 10
         .rev()
@@ -43,52 +43,52 @@ fn calc_output_values(charset_map: [Option<CharSet>; 10], outputs: &Vec<CharSet>
         .sum()
 }
 
-fn determine_charset_map(inputs: &mut Vec<CharSet>) -> [Option<CharSet>; 10] {
-    let mut codes: [Option::<CharSet>; 10] = Default::default();
+fn determine_segment_map(inputs: &mut Vec<Segments>) -> [Option<Segments>; 10] {
+    let mut segment_map: [Option::<Segments>; 10] = Default::default();
     // 1, 4, 7 and 8 have unique number of characters
     for (code_idx, target_length) in UNIQUE_LENGTHS_FOR_DIGIT {
-        codes[code_idx] = pop_charset_if(inputs, |charset|
-            charset.len() == target_length
+        segment_map[code_idx] = pop_segments_if(inputs, |unmatched_segment_set|
+            unmatched_segment_set.len() == target_length
         );
     }
 
     // 6 is the only 6-length set where the intersection with 7 is 2 chars
-    codes[6] = pop_charset_if(inputs, |charset|
-        length_and_overlap_pred(charset, 6, &codes[7], 2)
+    segment_map[6] = pop_segments_if(inputs, |unmatched_segment_set|
+        length_and_overlap_pred(unmatched_segment_set, 6, &segment_map[7], 2)
     );
 
     // 9 is the only 6-length set where the intersection with 4 is 4 chars
-    codes[9] = pop_charset_if(inputs, |charset|
-        length_and_overlap_pred(charset, 6, &codes[4], 4)
+    segment_map[9] = pop_segments_if(inputs, |unmatched_segment_set|
+        length_and_overlap_pred(unmatched_segment_set, 6, &segment_map[4], 4)
     );
 
     // 0 is the last remaining 6-length
-    codes[0] = pop_charset_if(inputs, |charset| charset.len() == 6);
+    segment_map[0] = pop_segments_if(inputs, |unmatched_segment_set| unmatched_segment_set.len() == 6);
 
     // 3 is the only 5-length set where the intersection with 7 is 3 chars
-    codes[3] = pop_charset_if(inputs, |charset|
-        length_and_overlap_pred(charset, 5, &codes[7], 3)
+    segment_map[3] = pop_segments_if(inputs, |unmatched_segment_set|
+        length_and_overlap_pred(unmatched_segment_set, 5, &segment_map[7], 3)
     );
 
     // 5 is the only 5-length set where the intersection with 6 is 5 chars
-    codes[5] = pop_charset_if(inputs, |charset|
-        length_and_overlap_pred(charset, 5, &codes[6], 5)
+    segment_map[5] = pop_segments_if(inputs, |unmatched_segment_set|
+        length_and_overlap_pred(unmatched_segment_set, 5, &segment_map[6], 5)
     );
 
     // 2 is the last remaining set
-    codes[2] = Some(inputs.remove(0));
+    segment_map[2] = Some(inputs.remove(0));
 
-    codes
+    segment_map
 }
 
-fn length_and_overlap_pred(charset: &CharSet, length: usize, overlap_set: &Option<CharSet>, overlap_length: usize) -> bool {
-    charset.len() == length
-    && charset.intersection(overlap_set.as_ref().unwrap()).count() == overlap_length
+fn length_and_overlap_pred(segments: &Segments, length: usize, overlap_set: &Option<Segments>, overlap_length: usize) -> bool {
+    segments.len() == length
+    && segments.intersection(overlap_set.as_ref().unwrap()).count() == overlap_length
 }
 
-fn pop_charset_if<F>(charsets: &mut Vec<CharSet>, condition: F) -> Option<CharSet>
-where F: FnMut(&CharSet) -> bool {
-    Some(charsets.remove(charsets.iter()
+fn pop_segments_if<F>(segmentss: &mut Vec<Segments>, condition: F) -> Option<Segments>
+where F: FnMut(&Segments) -> bool {
+    Some(segmentss.remove(segmentss.iter()
         .position(condition)
         .unwrap()
     ))
@@ -103,10 +103,10 @@ fn split_input_output(words: Vec<Vec<String>>) -> Vec<ProblemLine> {
         .collect::<Vec<ProblemLine>>()
 }
 
-fn as_char_sets(words: &[String]) -> Vec<CharSet> {
+fn as_char_sets(words: &[String]) -> Vec<Segments> {
     words.iter()
         .map(|word| word.chars()
-            .collect::<CharSet>()
+            .collect::<Segments>()
         )
-        .collect::<Vec<CharSet>>()
+        .collect::<Vec<Segments>>()
 }
