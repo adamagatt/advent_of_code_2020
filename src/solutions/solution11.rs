4c3ad7437fc84d39be10a1fe324ed59417939e7d
Adam Gatt <adam.gatt@live.com.au>
2021-12-11 19:19:01 +1030

    Cleanup and commenting

--- src/solutions/solution11.rs
+++ src/solutions/solution11.rs
@@ -1,6 +1,7 @@
 use crate::utils::read_2d_int_array;
 
 type OctopusGraph = Vec<Vec<u8>>;
+type FlashGraph = Vec<Vec<bool>>;
 
 pub fn solution11() -> () {
     let octopi = read_2d_int_array("src/data/solution11.txt");
@@ -18,8 +19,7 @@ fn solution11a(octopi_in: &OctopusGraph) -> usize {
     flashes
 }
 
-fn solution11b(octopi_in: OctopusGraph) -> usize {
-    let mut octopi = octopi_in.iter().cloned().collect::<OctopusGraph>();
+fn solution11b(mut octopi: OctopusGraph) -> usize {
     let num_octopi = octopi.len() * octopi[0].len();
 
     // Loop forever, while retaining index (first round is index 1)
@@ -53,15 +53,18 @@ fn run_simulation(mut octopi: &mut OctopusGraph) -> usize {
 
 fn advanced(octopi: &mut OctopusGraph) -> () {
     octopi.iter_mut()
-    .flat_map(|line| line.iter_mut())
-    .for_each(|value| *value += 1);
+        .flat_map(|line| line.iter_mut())
+        .for_each(|value| *value += 1);
 }
 
-fn ready_to_flash(octopi: &OctopusGraph, flashed_graph: &Vec<Vec<bool>>) -> Option<Vec<(usize, usize)>> {
+fn ready_to_flash(octopi: &OctopusGraph, flashed_graph: &FlashGraph) -> Option<Vec<(usize, usize)>> {
     let flash_list = octopi.iter()
         .enumerate()
         .flat_map(|(row_idx, row)| row.iter()
             .enumerate()
+            // "move" required to force closure to take ownership of row_idx, as the compiler suggests
+            // that the closure may outlive the row_idx reference, although it should be executed within
+            // the filter_map right here immediately
             .filter_map(move |(col_idx, &octopus)| if !flashed_graph[row_idx][col_idx] && octopus > 9 {
                 Some((row_idx, col_idx))
             } else {
@@ -70,6 +73,7 @@ fn ready_to_flash(octopi: &OctopusGraph, flashed_graph: &Vec<Vec<bool>>) -> Opti
         )
         .collect::<Vec<(usize, usize)>>();
 
+    // If there are no flash locations we prefer to return None than an empty Vec
     if flash_list.len() > 0 {
         Some(flash_list)
     } else {
@@ -80,7 +84,9 @@ fn ready_to_flash(octopi: &OctopusGraph, flashed_graph: &Vec<Vec<bool>>) -> Opti
 fn increase_around_flash(octopi: &mut OctopusGraph, row: usize, col: usize) -> () {
     for neighbour_row in usize::saturating_sub(row, 1)..=(row+1) {
         for neighbour_col in usize::saturating_sub(col, 1)..=(col+1) {
+            // Check within bounds (no need to check >0 due to saturating_sub)
             if neighbour_row < octopi.len() && neighbour_col < octopi[0].len()
+            // Shouldn't increase own counter (though this might not matter)
             && !(neighbour_row == row && neighbour_col == col) {
                 octopi[neighbour_row][neighbour_col] += 1;
             } 
@@ -88,9 +94,10 @@ fn increase_around_flash(octopi: &mut OctopusGraph, row: usize, col: usize) -> (
     }
 }
 
-fn reset_flashed(octopi: &mut OctopusGraph, flashed_graph: Vec<Vec<bool>>) -> () {
+fn reset_flashed(octopi: &mut OctopusGraph, flashed_graph: FlashGraph) -> () {
     octopi.iter_mut()
         .zip(flashed_graph.iter())
+        // Flatten graph to address memory locations in a linear iteration
         .flat_map(|(octopi_line, flashed_line)| octopi_line.iter_mut()
             .zip(flashed_line.iter())
         )
