4832964c3afaa5a88d906d73e8730474374fa55c
Adam Gatt <adam.gatt@live.com.au>
2022-08-28 14:40:35 +0930

    Option to run all solutions

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,27 @@ use std::io;
 use solutions::{make_choice_string, SOLVED_PROBLEMS};
 
 fn main() {
-    solutions::run(make_selection());
-}
-
-fn make_selection() -> i32 {
-    println!("Select a problem ({}):", make_choice_string());
+    println!("Select a problem ({}, [a]ll):", make_choice_string());
     let mut line = String::new();
     loop {
         match io::stdin().read_line(&mut line) {
             Ok(_) => {
-                if let Ok(choice) = line.trim().parse::<i32>() {
+                if line.trim().eq_ignore_ascii_case("a") || line.trim().eq_ignore_ascii_case("all") {
+                    SOLVED_PROBLEMS.iter().for_each(
+                        |(idx, solution)| {
+                            println!("Solution {}:", idx);
+                            solution();
+                            println!();
+                        }
+                    )
+                } else if let Ok(choice) = line.trim().parse::<i32>() {
                     if SOLVED_PROBLEMS.contains_key(&choice) {
-                        return choice;
+                        solutions::run(choice);
                     } else {
                         println!("Invalid number");
                     }
                 } else {
-                    println!("Please enter a number")
+                    println!("Please enter a number or \"all\"")
                 }
             }
             Err(error) => println!("Error reading input: {}", error),
